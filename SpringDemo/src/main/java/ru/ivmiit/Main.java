ebb36c0946bfad1857be541a5bbdbb9bc6b74cd3
bzalyaliev <bzalyaliev@gmail.com>
2021-08-28 20:32:03 +0300

    SpringDemo

---
+++ SpringDemo/src/main/java/ru/ivmiit/Main.java
@@ -0,0 +1,13 @@
+package ru.ivmiit;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new
+                ClassPathXmlApplicationContext("ru.ivmiit\\context.xml");
+        MessageRenderer renderer = (MessageRenderer) context.getBean("renderer");
+        renderer.printMessage();
+    }
+}
